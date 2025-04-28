851cbf38d8d48f5012531b90d5ce060f3a2f5738
Yash <yash.kaushik@highpointe.tech>
2025-04-28 12:27:06 +0530

    CI/CD pipeline setup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: "./postcss.config.js",
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    sourcemap: false,
+  },
+  base: "/", // Set this to your CloudFront distribution path if needed
 });
