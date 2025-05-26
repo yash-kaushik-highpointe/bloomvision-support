0a3caf00a574360c514e8454d9d1eae06b973e98
Yash <yash.kaushik@highpointe.tech>
2025-05-26 11:05:39 +0530

    Polotno Changes

--- src/components/Polotno/RightPanel/Photos/index.jsx
+++ src/components/Polotno/RightPanel/Photos/index.jsx
@@ -1,17 +1,34 @@
-import { useState } from "react";
+import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
+import PhotosList from "./PhotosList";
 import CategoryDropdown from "../../../Dropdown";
 import categories from "../../../../data/flowerCategories.json";
 
+import { fetchFlowersByCategory } from "../../../../store/slices/flowersSlice";
+
 function Photos() {
+  const dispatch = useDispatch();
+
+  const { flowersByCategory, loading } = useSelector((state) => state.flowers);
+
   const [selectedCategory, setSelectedCategory] = useState(categories[0].id);
 
   const handleCategoryChange = (...args) => {
     setSelectedCategory(...args);
   };
 
+  useEffect(() => {
+    if (!flowersByCategory[selectedCategory]) {
+      dispatch(fetchFlowersByCategory(selectedCategory))
+        .unwrap()
+        .catch((_) => toast.error("Failed to fetch Photos"));
+    }
+  }, [selectedCategory, dispatch, flowersByCategory]);
+
   return (
-    <div className="w-full">
+    <div className="w-full h-full">
       <CategoryDropdown
         className="mt-1"
         bgColor="#e3e6d3"
@@ -20,6 +37,11 @@ function Photos() {
         onChange={handleCategoryChange}
       />
       <hr className="w-[calc(100%+1rem)] -mx-2 my-3 border-t-2 border-[#cdd1bc]" />
+      <PhotosList
+        loading={loading}
+        selectedCategory={selectedCategory}
+        images={flowersByCategory[selectedCategory]}
+      />
     </div>
   );
 }
--- src/components/Polotno/utils.js
+++ src/components/Polotno/utils.js
@@ -10,3 +10,34 @@ export const filterRightPanel = (sections) => {
 
   return filteredSections;
 };
+
+const SquareComponents = {
+  frame: true,
+  bulk: true,
+  filler: true,
+  largeRound: true,
+  smallRound: true,
+  mediumRound: true,
+  urn: false,
+  vase: false,
+  stem: false,
+  ribbon: false,
+  lateral: false,
+  dancing: false,
+  drapping: false,
+  largeLateral: false,
+  LargeDrapping: false,
+};
+
+export const getPhotoStyles = (selectedCategory) => {
+  if (SquareComponents[selectedCategory])
+    return {
+      width: "146px",
+      height: "146px",
+    };
+  else
+    return {
+      width: "146px",
+      height: "438px",
+    };
+};
